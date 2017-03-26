8a55c00eac018d61001ae87118716f45f9af32dc
Vy Thuy Nguyen <vng0510@gmail.com>
2017-03-25 17:16:18 -0700

    test files

--- Q1/src/main/java/edu/gatech/cse6242/WordCount.java
+++ Q1/src/main/java/edu/gatech/cse6242/WordCount.java
@@ -13,53 +13,22 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class Q1 {
-
-	public static void main(String[] args) throws Exception {
-//		Configuration conf = new Configuration();
-//		Job job = Job.getInstance(conf, "Q1");
-//
-//		/* TODO: Needs to be implemented */
-//
-//		FileInputFormat.addInputPath(job, new Path(args[0]));
-//		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-//		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		
-		
-		
-		Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "Q1");
-	    
-	    
-	    job.setJarByClass(WordCount.class);
-	    job.setMapperClass(TokenizerMapper.class);
-	    job.setCombinerClass(IntSumReducer.class);
-	    job.setReducerClass(IntSumReducer.class);
-	    
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
-	    
-	    
-	    FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	    System.exit(job.waitForCompletion(true) ? 0 : 1);
-		
-		
-		
-		
-	}
+public class WordCount 
+{
 
 	public static class TokenizerMapper extends
-			Mapper<Object, Text, Text, IntWritable> {
+			Mapper<Object, Text, Text, IntWritable> 
+	{
 
 		private final static IntWritable one = new IntWritable(1);
-		private Integer nodeId;
-		
+		private Text word = new Text();
+
 		public void map(Object key, Text value, Context context)
-				throws IOException, InterruptedException {
+				throws IOException, InterruptedException 
+		{
 			StringTokenizer itr = new StringTokenizer(value.toString());
-			
-			while (itr.hasMoreTokens()) {
+			while (itr.hasMoreTokens()) 
+			{
 				word.set(itr.nextToken());
 				context.write(word, one);
 			}
@@ -80,4 +49,19 @@ public class Q1 {
 			context.write(key, result);
 		}
 	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
 }
