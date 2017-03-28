728485d272b94f9dfa724341c7d7973c77ce278a
Vy Thuy Nguyen <vng0510@gmail.com>
2017-03-27 20:10:19 -0700

    finalize

--- Q4/run1.sh
+++ Q4/run1.sh
@@ -1,3 +1,3 @@
-hadoop jar ./target/q4-1.0.jar edu.gatech.cse6242.Q4 /user/cse6242/small.tsv /user/cse6242/q4outputsm
+hadoop jar ./target/q4-1.0.jar edu.gatech.cse6242.Q4 /user/cse6242/test.tsv /user/cse6242/q4outputsm
 hadoop fs -getmerge /user/cse6242/q4outputsm/ q4outputsm.tsv
-hadoop fs -rm -r /user/cse6242/q1output1
+hadoop fs -rm -r /user/cse6242/q4outputsm
--- Q4/src/main/java/edu/gatech/cse6242/Q4.java
+++ Q4/src/main/java/edu/gatech/cse6242/Q4.java
@@ -19,34 +19,35 @@ public class Q4
 {
   public static void main(String[] args) throws Exception 
   {
-	  degreeJob(args[0], args[1]);
-	  //frequencyJob(tmpRes, args[1]);
+	  String tmpRes = degreeJob(args[0]);
+	  frequencyJob(tmpRes, args[1]);
   }
   
-//  public void frequencyJob(String inputFile, String outputFile)
-//  {
-//	  Configuration conf = new Configuration();
-//	  Job job = Job.getInstance(conf, "Q4");
-//	
-//	  job.setJarByClass(Q4.class);
-//	  job.setMapperClass(DegreeMapper.class);
-//	  job.setCombinerClass(DegreeReducer.class);
-//	  job.setReducerClass(DegreeReducer.class);
-//	    
-//	  job.setOutputKeyClass(Text.class);
-//	  job.setOutputValueClass(IntWritable.class);
-//	    
-//	  FileInputFormat.addInputPath(job, new Path(inputFile));
-//	  FileOutputFormat.setOutputPath(job, new Path(outputFile));
-//	  System.exit(job.waitForCompletion(true) ? 0 : 1);
-//  }
+  public void frequencyJob(String inputFile, String outputFile)
+			throws IOException, InterruptedException, ClassNotFoundException
+  {
+	  Configuration conf = new Configuration();
+	  Job job = Job.getInstance(conf, "Q4");
+	
+	  job.setJarByClass(Q4.class);
+	  job.setMapperClass(FrequencyMapper.class);
+	  job.setCombinerClass(FrequencyReducer.class);
+	  job.setReducerClass(FrequencyReducer.class);
+	    
+	  job.setOutputKeyClass(Text.class);
+	  job.setOutputValueClass(IntWritable.class);
+	    
+	  FileInputFormat.addInputPath(job, new Path(inputFile));
+	  FileOutputFormat.setOutputPath(job, new Path(outputFile));
+	  System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
   
   //Return the output filename to pass to next job
   //Tag (append to keep the path) with timestamp so don't have to del files 
-  public static void degreeJob(String inputFile, String outputFile)
+  public static String degreeJob(String inputFile)
   				throws IOException, InterruptedException, ClassNotFoundException
   {
-	  //String outputFile = String.format("%s_%s.tsv", inputFile, System.currentTimeMillis());
+	  String outputFile = String.format("%s_%s.tsv", inputFile, System.currentTimeMillis());
 	  
 	  Configuration conf = new Configuration();
 	  Job job = Job.getInstance(conf, "Q4");
@@ -63,22 +64,49 @@ public class Q4
 	  FileOutputFormat.setOutputPath(job, new Path(outputFile));
 	  System.exit(job.waitForCompletion(true) ? 0 : 1);
 	  
-	  //return outputFile;
+	  return outputFile;
   }
   
-//  public static class FrequencyMapper
-//  			extends Mapper<Object, Text, IntWritable, IntWritable>
-//  {
-//	  private final static IntWritable one = new IntWritable(1);
-//		
-//	  @Override
-//	  public void map(Object key, Text value, Context context)
-//				throws IOException, InterruptedException
-//	  {
-//		  //<node id> <degree>
-//		  String[] toks = value.toString();
-//	  }
-//  }
+  //Key degree; val count
+  public static class FrequencyMapper
+  			extends Mapper<Object, Text, Text, IntWritable>
+  {
+	  private final static IntWritable one = new IntWritable(1);
+		
+	  @Override
+	  public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException
+	  {
+		  //<node id> <degree>
+		  String[] toks = value.toString().split("\\s+");
+		  
+		  //Skip ill-formatted lines
+		  if (toks.length == 2)
+		  {
+			  context.write(new Text(toks[1]), one);
+			  
+		  }
+	  }
+  }
+
+  //Input: key: node id, val: degree
+  //Output: key: degree, val: freq
+  public static class FrequencyReducer
+		extends Reducer<Text, IntWritable, Text, IntWritable>
+  {
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException 
+		{
+			int sum = 0;
+			for (IntWritable val : values) 
+			{
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	
+  }
   
   //Mapper<KeyIn, ValueInt, KeyOut, ValueOut>
   //Key: node ID; Value: degree
